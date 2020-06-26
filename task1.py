dc828bd7620474516cd1d536a4b48c6a10fdbc5a
nix30fix2879 <58103512+nix30fix2879@users.noreply.github.com>
2020-06-26 17:43:08 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,61 +8,61 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-# Given jab code order
-jab_code_order = ['J92.9', 'A10.2', 'E24.9', 'B10.2', 'F19.10', 'D10.11']
-
-# instantiate driver
-driver = webdriver.Chrome('C:\\chrome\\chromedriver.exe')
-
-# URL
-url = 'C:\\chrome\\index.html'
-
 
 class JabCodeOrdering:
+    def __init__(self):
+        # Given jab code order
+        self.jab_code_order = ['J92.9', 'A10.2', 'E24.9', 'B10.2', 'F19.10', 'D10.11']
+
+        # instantiate driver
+        self.driver = webdriver.Chrome('C:\\chrome\\chromedriver.exe')
+
+        # URL
+        self.url = 'C:\\chrome\\index.html'
 
     def setupDriver(self):
         # Maximize browser window
-        driver.maximize_window()
+        self.driver.maximize_window()
         # html file where jab codes are present
-        driver.get(url)
+        self.driver.get(self.url)
 
     # get the list of all jab code present in  web page
     def get_current_element_order(self):
-        array_of_elements = driver.find_elements_by_xpath("//tbody//tr")
+        array_of_elements = self.driver.find_elements_by_xpath("//tbody//tr")
         return array_of_elements
 
     # Arrange jab code on given sorted order based on x,y offset
     def arrangeJabCodes(self):
-        for item in range(len(jab_code_order)):
+        for item in range(len(self.jab_code_order)):
             # retrieve jab code from webpage
-            row = driver.find_element_by_xpath(
-                "//tr[@class='ui-sortable-handle']//td[text()='" + jab_code_order[item] + "']")
+            row = self.driver.find_element_by_xpath(
+                "//tr[@class='ui-sortable-handle']//td[text()='" + self.jab_code_order[item] + "']")
             # find source element from given jab code list
-            source_element = driver.find_element_by_xpath(
-                "//tr[@class='ui-sortable-handle']//td[text()='" + jab_code_order[item] + "']")
+            source_element = self.driver.find_element_by_xpath(
+                "//tr[@class='ui-sortable-handle']//td[text()='" + self.jab_code_order[item] + "']")
             # get current order of elements
             current_order_of_elements = self.get_current_element_order()
             dest_element = current_order_of_elements[item]
             # Drag source elements to destination using negative offsets
             if dest_element.location['y'] - source_element.location['y'] < 0:
-                ActionChains(driver).drag_and_drop_by_offset(source_element,
-                                                             0,
-                                                             dest_element.location['y'] -
-                                                             source_element.location[
-                                                                 'y'] - 5).perform()
+                ActionChains(self.driver).drag_and_drop_by_offset(source_element,
+                                                                  0,
+                                                                  dest_element.location['y'] -
+                                                                  source_element.location[
+                                                                      'y'] - 5).perform()
             else:
                 # Drag source elements to destination using offsets
-                ActionChains(driver).drag_and_drop_by_offset(source_element,
-                                                             0,
-                                                             dest_element.location['y'] -
-                                                             source_element.location[
-                                                                 'y']).perform()
+                ActionChains(self.driver).drag_and_drop_by_offset(source_element,
+                                                                  0,
+                                                                  dest_element.location['y'] -
+                                                                  source_element.location[
+                                                                      'y']).perform()
             time.sleep(2)
 
     # Terminate driver instance
     def terminateDriver(self):
-        driver.close()
-        driver.quit()
+        self.driver.close()
+        self.driver.quit()
 
 
 if __name__ == '__main__':
